bd8640e4a565ef5aa60d62e78d3e585a6da9e92b
xiepingfu <lightxpf@gmail.com>
2017-12-03 19:35:48 +0800

    稳定版本

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
+    path('study', views.study, name='study'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -13,3 +13,6 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def study(request):
+    return render(request, 'study.html')
